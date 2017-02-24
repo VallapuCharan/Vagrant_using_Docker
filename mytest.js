5a2f8c9e3c1ca5d2f054f2088dde5dae85d14864
VallapuCharan <vallapucharan@gmail.com>
2017-02-24 10:05:37 +0530

    Update mytest.js

--- mytest.js
+++ mytest.js
@@ -1,9 +1,9 @@
 describe('angularjs homepage todo list', function() {
     it('should add a todo', function() {
 		console.log("hello");
-        browser.get('https://core.opentext.com/users/#/signin');
-        element(by.id('emailInput')).sendKeys('svallapu@opentext.com');
-        element(by.id('passwordInput')).sendKeys('Smokers123$');
+        browser.get('xxx');
+        element(by.id('emailInput')).sendKeys('xxxx');
+        element(by.id('passwordInput')).sendKeys('xxxx');
        element(by.css('.btn.btn-default')).click();
        browser.sleep(5000);
 
